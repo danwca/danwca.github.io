c7a25e290926a486265e158a7668e0ad63158163
Daniel Wang <78062882+danwca@users.noreply.github.com>
2024-05-01 00:13:34 -0400

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,15 @@
+
+Node.js
+
+#### 方案
+1. 转换为js，可用于浏览器，建立客户端应用程序； webpack, 
+2. 建立网站服务器， 使用数据库， 
+
+#### 概念
+1. 页面流程， 模板， 栏目， 内容等；
+
+#### 参考库
+1. Refine, [Github](https://github.com/refinedev/refine)
+2. webpack, 
+3. react,
+                    
